243e6f765c2479acb305f39737b61227195b14c9
minicl55 <whatsittoya573@gmail.com>
2014-12-22 22:32:46 -0500

    Made code work

--- cloud/app.js
+++ cloud/app.js
@@ -8,215 +8,36 @@ app.set('views', 'cloud/views');
 app.set('view engine', 'ejs');
 app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
 app.use(express.bodyParser());
-app.use(express.cookieParser('CONVERTICS'));
+app.use(express.cookieParser('civicmon'));
 app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 
-app.get('/', function(req, res) {
-	res.render('login.ejs');
-});
+app.post('/submit', function(req, res) {
+	console.log('started');
+	console.log(req.body);
+	console.log(req.param('legislator'));
+	console.log(req.param('action'));
+	console.log(req.param('bill'));
+	console.log(req.param('notes'));
+	
+	var Activity = Parse.Object.extend("Activity");
+	var activity = new Activity();
 
-app.get('/welcome', function(req, res) {
-	res.render('welcome.ejs');
-});
-
-app.post('/gotosignup', function(req, res) {
-	res.redirect('/signup');
-});
-
-app.post('/gotologin', function(req, res) {
-	res.redirect('/login');
-});
-
-app.get('/login', function(req, res) {
-	res.render('login.ejs');
-});
-
-// Clicking submit on the login form triggers this.
-app.post('/login', function(req, res) {
-	Parse.User.logIn(req.body.username, req.body.password).then(function() {
-	// Login succeeded, redirect to contributors.
-	// parseExpressCookieSession will automatically set cookie.
-		res.redirect('/navigation');
-		//res.render('contributors-parsetable.ejs');
-	},
-	function(error) {
-		  // Login failed, redirect back to login form.
-		  res.redirect('/login');
-	});
-});
-
-app.get('/signup', function(req, res) {
-	res.render('signup.ejs');
-});
-
-app.post('/logout', function(req, res) {
-	Parse.User.logOut();
-	res.redirect('/login');
-});
-
-app.get('/contributors', function(req, res) {
-
-	if (Parse.User.current()) {
-	  // No need to fetch the current user for querying Note objects.
-	  var Contributor = Parse.Object.extend("AllCallListNov11");
-	  var contQuery = new Parse.Query(Contributor);
-	  
-	  // console.log(JSON.stringify(req.cookies));
-	  // console.log(JSON.stringify(Parse.User.current().id));
-
-	  var userQuery = new Parse.Query(Parse.User);
-
-	  userQuery.get(Parse.User.current().id, function(user){
-	  	// console.log(JSON.stringify(user));
-	  	contQuery.equalTo("ID", user.getUsername());
-	  	contQuery.find().then(function(results) {
-		    // Render contributors
-		    // res.render('contributors.ejs', {responsedata:res, contributors:results});
-			// var attribs = _.map(results, function(each) { 
-			// 	// console.log(JSON.stringify(each));
-			// 	return each.attributes});
-		  	res.render('simpler-table.ejs', {contributors: results});
-	  
-		},
-		  function(error) {
-		    // Render error page.
-		    res.render(JSON.stringify(error));
-		  });
-	  });
-	} else {
-	  // Render a public welcome page, with a link to the '/login' endpoint.
-	  res.redirect('/welcome');
-	}
-  });
-
-// You could have a "Profile" link on your website pointing to this.
-app.get('/profile', function(req, res) {
-	// Display the user profile if user is logged in.
-	if (Parse.User.current()) {
-	  // We need to fetch because we need to show fields on the user object.
-	  Parse.User.current().fetch().then(function(user) {
-	    // Render the user profile information (e.g. email, phone, etc).
+	activity.set("legislator", req.param('legislator'));
+	activity.set("action", req.param('action'));
+	activity.set("bill", req.param('bill'));
+	activity.set("notes", req.param('notes'));
+	activity.set("event", req.param("event"));
+ 
+	activity.save(null, {
+	  success: function(activity) {
+		// Execute any logic that should take place after the object is saved.
+		console.log('success');
+		res.success();
 	  },
-	  function(error) {
-	    // Render error page.
-	  });
-	} else {
-	  // User not logged in, redirect to login form.
-	  res.redirect('/login');
-	}
-});
-
-
-app.post('/gotonavigation', function(req, res) {
-	res.redirect('/navigation');
-});
-
-app.get('/navigation', function(req, res) {
-	res.render('navigation.ejs');
-});
-
-app.post('/gotofaq', function(req, res) {
-	res.redirect('/faq');
-});
-
-app.get('/faq', function(req, res) {
-	res.render('faq.ejs');
-});
-
-app.post('/gotocontributors', function(req, res) {
-	res.redirect('/contributors');
-});
-
-app.post('/gotosummary', function(req, res) {
-	res.redirect('/summary');
-});
-
-app.post('/gotofullresults', function(req, res) {
-	res.redirect('/fullresults');
-});
-
-
-app.get('/summary', function(req, res) {
-
-	// var username = Parse.User.current().getUsername();
-	// console.log(JSON.stringify(username));	
-
-	if (Parse.User.current()) {
-	  	
-	  	console.log(JSON.stringify(Parse.User.current()));
-	  	var userID = Parse.User.current().id;
-		console.log(userID);
-		var candidate;
-		
-		// console.log(JSON.stringify());
-		//candidate=user.get("username");
-		
-		// var userQuery = new Parse.Query(Parse.User);
-
-		// Parse.User.current().fetch().then(function (user) {
-		// userQuery.get(Parse.User.current().id, function(user){
-		// console.log("userID" + userID);
-		// userQuery.get(userID).then(function (user) {
-		//   console.log("userID" + userID);
-		//   console.log("user" + JSON.stringify(user));
-		//   candidate=user.get("username");
-		// });
-	
-		var page = '/slides.html';
-	  	console.log('/' + userID + page);
-		res.redirect('/' + userID + page);
-	}
-});
-
-app.get('/fullresults', function(req, res) {
-
-	// var username = Parse.User.current().getUsername();
-	// console.log(JSON.stringify(username));	
-
-	if (Parse.User.current()) {
-	  	
-	  	console.log(JSON.stringify(Parse.User.current()));
-	  	var userID = Parse.User.current().id;
-		console.log(userID);
-		var candidate;
-		
-		// console.log(JSON.stringify());
-		//candidate=user.get("username");
-		
-		// var userQuery = new Parse.Query(Parse.User);
-
-		// Parse.User.current().fetch().then(function (user) {
-		// userQuery.get(Parse.User.current().id, function(user){
-		// console.log("userID" + userID);
-		// userQuery.get(userID).then(function (user) {
-		//   console.log("userID" + userID);
-		//   console.log("user" + JSON.stringify(user));
-		//   candidate=user.get("username");
-		// });
-	
-		var page = '/fullresults.csv';
-	  	console.log('/' + userID + page);
-		res.redirect('/' + userID + page);
-	}
-});
-
-app.get('/submit', function(req, res) {
-	var newData = Parse.Object.extend("newData");
-	var newData = new newData();
- 
-	newData.set("senator", request.body.user.senator);
-	newData.set("action", request.body.user.action);
-	newData.set("bill", request.body.user.bill);
- 
-newData.save(null, {
-  success: function(newData) {
-    // Execute any logic that should take place after the object is saved.
-    alert('Data saved');
-  },
-  error: function(newData, error) {
-    // Execute any logic that should take place if the save fails.
-    alert('Failed to save data, please try again'); //Developer errors shouldn't be in release builds 
-  }
+	  error: function(activity, error) {
+      // Execute any logic that should take place if the save fails.
+      console.log('Failed to save data, please try again'); //Developer errors shouldn't be in release builds 
+    }});
 });
 
 app.listen();
